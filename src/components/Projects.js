89e15b30e299ea6cc81d13e38c8a9f10b6356ab3
Kajal Bhatt <k13bhatt@gmail.com>
2023-02-27 19:29:31 +0530

    committed files

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,23 +8,23 @@ const Projects = () => {
         <h3>PROJECTS</h3>
         <div className="project-items">
           <div className="item-card">
-            <a href='https://google.com'><img src={require("../img/img2.jpg")} alt="A project" /></a>
+            <a href='https://image-finder-kappa.vercel.app/'><img src={require("../img/imagefinder.png")} alt="A project" /></a>
                          
-            <h2>Project name</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, illo?</p>
+            <h2>Image Finder</h2>
+            <p>Image finder made with react and tailwind that uses pixabay api to generate images</p>
             <div className="links">
-              <a href='https://google.com' className='s-link'>Demo</a>
-              <a href='https://github.com'><i className='fa fa-github s-liink'></i></a>
+              <a href='https://image-finder-kappa.vercel.app/' className='s-link'>Demo</a>
+              <a href='https://github.com/K13BT/Image_Finder'><i className='fa fa-github s-liink'></i></a>
             </div>
             
           </div>
           <div className="item-card">
-            <a href='https://google.com'><img src={require("../img/img3.jpg")} alt="A project" /> </a>   
-            <h2>Project name</h2>
+            <a href='https://expense-tracker-k13bt.vercel.app/'><img src={require("../img/expense.png")} alt="A project" /> </a>   
+            <h2>Expnese Tracker</h2>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, illo?</p> 
             <div className="links">
-              <a href='https://google.com' className='s-link'>Demo</a>
-              <a href='https://github.com'><i className='fa fa-github s-link'></i></a>
+              <a href='https://expense-tracker-k13bt.vercel.app/' className='s-link'>Demo</a>
+              <a href='https://github.com/K13BT/Expense-tracker'><i className='fa fa-github s-link'></i></a>
             </div>
           </div>
         </div>
